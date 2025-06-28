347dace94dc8d715844b77e06ce53b14a41d7de8
Héctor Chocobar-Torrejón <45291267+hchocobar@users.noreply.github.com>
2025-06-28 11:28:28 +0000

    db-init

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 db = SQLAlchemy()
@@ -8,32 +9,53 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    first_name = db.Column(db.String, nullable=True)
-    last_name =  db.Column(db.String, nullable=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    is_active = db.Column(db.Boolean, nullable=False)
+    is_admin = db.Column(db.Boolean, nullable=False)
+
+
+class Products(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(300), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+
+class Bills(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    total_price = db.Column(db.Float, nullable=False)
+    bill_address = db.Column(db.String(180), nullable=False)
+    delivery_address = db.Column(db.String(180), nullable=False)
+    status = db.Column(db.Enum('pending', 'paid', 'cancel', name='status'), nullable=False)
+    payment_method = db.Column(db.Enum('visa', 'amex', 'paypal', name='payment_method'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_to = db.relationship('Users', foreign_keys=[user_id],
+                               backref=db.backref('bill_to', lazy='select'))
+
+
+class BillItems(db.Model):
+    __tablename__ = 'bill_items'
+    id = db.Column(db.Integer, primary_key=True)
+    price_per_unit = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    bill_id = db.Column(db.Integer, db.ForeignKey('bills.id'))
+    bill_to = db.relationship('Bills', foreign_keys=[bill_id],
+                                  backref=db.backref('bill_item_to', lazy='select'))
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+    product_to = db.relationship('Products', foreign_keys=[product_id],
+                                  backref=db.backref('bill_item_to', lazy='select'))
+
+
+class Followers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    following_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    following_to = db.relationship('Users', foreign_keys=[following_id], 
+                                   backref=db.backref('following_to', lazy='select'))
+    follower_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    follower_to = db.relationship('Users', foreign_keys=[follower_id], 
+                                   backref=db.backref('follower_to', lazy='select'))
 
     def __repr__(self):
-        return f'<User {self.email}>'
-
-    def serialize(self):
-        # do not serialize the password, its a security breach
-        return {'id': self.id,
-                'email': self.email,
-                'is_active': self.is_active,
-                'first_name': self.first_name,
-                'last_name': self.last_name}
-    
-
-class Posts(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    description = db.Column(db.String)
-    body = db.Column(db.String)
-    date = db.Column(db.Date, nullable=False)
-    image_url = db.Column(db.String)
-
-
-class Media(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    media_type = db.Column(db.Enum('image', 'video', 'audio', name='media_type'), nullable=False)
-    url = db.Column(db.String)
+        return f'following: {self.following_id} - follower: {self.follower_id}'    
