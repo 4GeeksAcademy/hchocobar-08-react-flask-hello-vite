a41b134022004314ce0d7bab92c07cf470a4a4a1
Héctor Chocobar-Torrejón <45291267+hchocobar@users.noreply.github.com>
2026-05-15 16:25:19 +0000

    navbar

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,9 +1,20 @@
 import { Link } from "react-router-dom";
+import useGlobalReducer from "../hooks/useGlobalReducer";
 
 export const Navbar = () => {
+	//  const { store, dispatch } = useGlobalReducer();
+	const { store, dispach } = useGlobalReducer()
 
 	return (
 		<nav className="navbar navbar-light bg-light">
+			Cohorte {store.cohorte}
+				<div className="ml-auto">
+					<Link to="/users">
+						<button className="btn btn-primary">Users</button>
+					</Link>
+				</div>
+
+			{/* 
 			<div className="container">
 				<Link to="/">
 					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
@@ -14,6 +25,7 @@ export const Navbar = () => {
 					</Link>
 				</div>
 			</div>
-		</nav>
+ 			*/}		
+ 		</nav>
 	);
 };
